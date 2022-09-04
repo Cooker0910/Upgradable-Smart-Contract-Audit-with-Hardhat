1af489ea278d38616b5624e87f15f62c8debb328
cooker0910 <cooker0910@gmail.com>
2022-09-04 09:13:54 -0700

    update

--- test/3.BoxProxyV2.test.js
+++ test/3.BoxProxyV2.test.js
@@ -1,15 +1,16 @@
 const { expect } = require("chai")
 const { ethers, upgrades } = require("hardhat")
-require('@openzeppelin/hardhat-upgrades');
+const {BigNumber} = require("ethers")
 
 describe("Box Proxy V2", () => {
+  let boxV2;
   beforeEach(async() => {
     const Box = await ethers.getContractFactory("Box")
     const BoxV2 = await ethers.getContractFactory("BoxV2")
 
     const box = await upgrades.deployProxy(Box, [42], {initializer: "store"})
 
-    const boxV2 = await upgrades.upgradeProxy(box.address, BoxV2)
+    boxV2 = await upgrades.upgradeProxy(box.address, BoxV2)
   })
 
   it("should retrieve value previously stored and increment correctly", async function () {
